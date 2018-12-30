dbf72b34b8fd11e5c05a6953695f14cdfda0ed3e
喵喵mya <37319666+myacat@users.noreply.github.com>
2018-12-31 03:22:44 +0800

    添加一个vector类型的UI样式

--- Assets/Script/MyaShaderDrawer.cs
+++ Assets/Script/MyaShaderDrawer.cs
@@ -48,5 +48,75 @@ namespace MyaShaderDrawer
         }
 
     }
+    internal class VectorFieldDrawer : MaterialPropertyDrawer
+    {
+        private readonly GUIContent[] labels;
+
+        private float labelWidth = 13f;
+        public VectorFieldDrawer(string x, float labelWidth) : this(labelWidth,new string[] { x }) { }
+        public VectorFieldDrawer(string x, string y, float labelWidth = 13f) : this(labelWidth,new string[] { x, y }) { }
+        public VectorFieldDrawer(string x, string y, string z, float labelWidth = 13f) : this(labelWidth,new string[] { x, y, z }) { }
+        public VectorFieldDrawer(string x, string y, string z, string w, float labelWidth = 13f) : this(labelWidth,new string[] { x, y, z, w }) { }
+        public VectorFieldDrawer(float labelWidth , params string[] labels)
+        {
+            this.labels = new GUIContent[labels.Length];
+            for (int i = 0; i < labels.Length; i++)
+            {
+                this.labels[i] = new GUIContent(labels[i]);
+                this.labelWidth = labelWidth;
+            }
+        }
+
+        public override void OnGUI(Rect position, MaterialProperty prop, GUIContent label, MaterialEditor editor)
+        {
+            if (prop.type == MaterialProperty.PropType.Vector)
+            {
+                position = EditorGUI.IndentedRect(position);
+
+
+                var value = prop.vectorValue;
+
+                EditorGUI.BeginChangeCheck();
+                {
+                    float interval = 2f;//每个分量之间的间距
+                    int eCount = labels.Length;
+                    float w = (position.width - (eCount - 1) * interval) / eCount;//计算每个分量的宽度
+                    Rect nr = new Rect(position) { width = w };
+
+                    //防止影响其他Properties的布局，把需要修改的参数存起来，绘制完自己的UI后复原
+                    float oldLabelWidth = EditorGUIUtility.labelWidth;
+                    int oldIndentLevel = EditorGUI.indentLevel;
+                    EditorGUIUtility.labelWidth = labelWidth;
+                    EditorGUI.indentLevel = 0;
+                    for (int i = 0; i < eCount; i++)
+                    {
+                        if (labelWidth < 1)
+                        {
+                            char[] q = labels[i].text.ToCharArray();
+                            EditorGUIUtility.labelWidth = q.Length * 7.5f;
+                        }
+
+                        value[i] = EditorGUI.FloatField(nr, labels[i], value[i]);
+                        nr.x += w + interval;
+                    }
+                    EditorGUIUtility.labelWidth = oldLabelWidth;
+                    EditorGUI.indentLevel = oldIndentLevel;
+                }
+
+                if (EditorGUI.EndChangeCheck())
+                {
+                    prop.vectorValue = value;
+
+                }
+                
+            }
+            else
+            {
+                editor.DefaultShaderProperty(prop, label.text);
+            }
+               
+        }
+
+    }
 }
 
--- Assets/Shader/IntDraweTest.shader
+++ Assets/Shader/IntDraweTest.shader
@@ -2,10 +2,16 @@
 {
 	Properties
 	{
-		_IntValue("float Value" , float) = 1
+
+		[PowerSlider(3)]_IntValue("float Value" , Range(0,10)) = 1
+				[VectorField(asdsd,bbbbbasdsadb,cccc,30)]_VectorValue("Vector" ,Vector) = (0,0,0,0)
+
 		_IntValue2("Int Value" , int) = 1
+
 		[Int]_IntValue3("float Value" , float) = 1
 		[Int]_IntValue4("Int Value" , int) = 1
+
+
 	}
 	SubShader
 	{
